c33fc4d95edd0be9e190cc219e02ceb03c120c99
RomulusED69 <redieux@gmail.com>
2019-07-11 15:11:05 +0200

    Correction unserialization debug tollbar

--- src/DataCollector/ClientRequest.php
+++ src/DataCollector/ClientRequest.php
@@ -1,15 +1,20 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Yproximite\Bundle\InfluxDbPresetBundle\DataCollector;
 
+use Yproximite\Bundle\InfluxDbPresetBundle\Connection\Connection;
+use Yproximite\Bundle\InfluxDbPresetBundle\Connection\ConnectionInterface;
+use Yproximite\Bundle\InfluxDbPresetBundle\Point\PointPreset;
+use Yproximite\Bundle\InfluxDbPresetBundle\Profile\Profile;
 use Yproximite\Bundle\InfluxDbPresetBundle\Profile\ProfileInterface;
 use Yproximite\Bundle\InfluxDbPresetBundle\Point\PointPresetInterface;
 
 /**
  * Class ClientRequest
  */
-class ClientRequest
+class ClientRequest implements \Serializable
 {
     /**
      * @var ProfileInterface
@@ -61,5 +66,81 @@ class ClientRequest
     public function getDateTime(): \DateTimeInterface
     {
         return $this->dateTime;
+    }
+
+    public function serialize()
+    {
+        return serialize([
+            $this->value,
+            $this->dateTime,
+            $this->profile->getName(),
+            array_map(function (ConnectionInterface $connection) {
+                return $this->serializeConnection($connection);
+            }, $this->profile->getConnections()),
+            array_map(function (PointPreset $pointPreset) {
+                return $this->serializePointPreset($pointPreset);
+            }, $this->profile->getPointPresets()),
+            $this->serializePointPreset($this->pointPreset),
+        ]);
+    }
+
+    public function unserialize($serialized)
+    {
+        list($this->value, $this->dateTime, $profileName, $profileConnections, $profilePointPresets, $pointPreset) = unserialize($serialized);
+
+        $this->pointPreset = $this->unserializePointPreset($pointPreset);
+
+        $this->profile = new Profile();
+        $this->profile->setName($profileName);
+
+        foreach ($profileConnections as $connection) {
+            $this->profile->addConnection($this->unserializeConnection($connection));
+        }
+
+        foreach ($profilePointPresets as $pointPreset) {
+            $this->profile->addPointPreset($this->unserializePointPreset($pointPreset));
+        }
+    }
+
+    protected function serializePointPreset(PointPresetInterface $pointPreset): array
+    {
+        return [
+            $pointPreset->getName(),
+            $pointPreset->getFields(),
+            $pointPreset->getMeasurement(),
+            $pointPreset->getTags(),
+        ];
+    }
+
+    protected function unserializePointPreset(array $pointPreset): PointPresetInterface
+    {
+        $newPointPreset = new PointPreset();
+        $newPointPreset->setName($pointPreset[0])
+            ->setFields($pointPreset[1])
+            ->setMeasurement($pointPreset[2])
+            ->setTags($pointPreset[3])
+        ;
+
+        return $newPointPreset;
+    }
+
+    protected function serializeConnection(ConnectionInterface $connection): array
+    {
+        return [
+            $connection->getName(),
+            $connection->getProtocol(),
+            $connection->isDeferred(),
+        ];
+    }
+
+    protected function unserializeConnection(array $connection): ConnectionInterface
+    {
+        $newConnection = new Connection();
+        $newConnection->setName($connection[0])
+            ->setProtocol($connection[1])
+            ->setDeferred($connection[2])
+        ;
+
+        return $newConnection;
     }
 }
